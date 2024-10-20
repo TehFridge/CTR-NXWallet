33553d93a745c2d604263f386d65056be892f534
TehFridge <85436576+TehFridge@users.noreply.github.com>
2024-10-20 19:47:04 +0200

    okey i admit. i used chatgpt while coding this.

--- love-main/main.lua
+++ love-main/main.lua
@@ -1,5 +1,5 @@
 require "lib.text-draw"
-font = love.graphics.newFont(12, "normal", 4) -- Font lol
+font = love.graphics.newFont(12, "normal", 4) 
 local json = require("lib.json")
 local qrcode = require('lib.loveqrcode')
 local https = require("https")
@@ -9,7 +9,7 @@ local bit = require("bit")
 local sha1 = require("sha1")
 local struct = require("lib.struct")
 local jsonread = true
-local code128 = require("lib.bar128") -- an awesome Code128 library made by Nawias (POLSKA GUROM)
+local code128 = require("lib.bar128") 
 local EAN13 = require("lib.ean13")
 local itfbarcode = require("lib.i25")
 local SCREEN_WIDTH = 400
@@ -25,24 +25,24 @@ local gonotfish = love.audio.newSource("assets/back.ogg", "static")
 local sfx2 = love.audio.newSource("assets/accept.ogg", "static")
 love.graphics.setDefaultFilter("nearest")
 kodyexist = love.filesystem.exists("kody.json")
--- Waveform settings
--- starfield.lua
+
+
 local stars = {}
 local numStars = 200
 local centerX, centerY
---sine scroll text (NX Only)
-local xPos
-local speed = 300 -- Speed of scrolling
 
-local frequency = 5 -- Frequency of sine wave
+local xPos
+local speed = 300 
+
+local frequency = 5 
 if love._potion_version == nil then
 	local nest = require("nest").init({ console = "switch", scale = 1 })
 	love._nest = true
     love._console_name = "Switch"
 end
 if love._console == "3DS" then
-    charWidth = 60 -- Fixed width for each character
-	amplitude = 25 -- Amplitude of sine wave
+    charWidth = 60 
+	amplitude = 25 
 	SCREEN_WIDTH = 400
 	SCREEN_HEIGHT = 240
 	QR_SCALE_BASE = 214.5
@@ -51,8 +51,8 @@ if love._console == "3DS" then
 	theme = "light"
 	text = "CTRWallet"
 elseif love._console == "Switch" then
-	charWidth = 120 -- Fixed width for each character
-	amplitude = 50 -- Amplitude of sine wave
+	charWidth = 120 
+	amplitude = 50 
 	SCREEN_WIDTH = 1280
 	SCREEN_HEIGHT = 720
 	QR_SCALE_BASE = 504.5
@@ -90,31 +90,31 @@ function love.load()
 	elseif love._console == "Wii U" then
 		table.insert(buttons, createButton(SCREEN_WIDTH / 1.4, SCREEN_WIDTH / 2.2, "assets/add.png", addcode, "main_page", "barcode"))
 	end
-	xPos = love.graphics.getWidth() -- Start position off-screen
+	xPos = love.graphics.getWidth() 
 	table.insert(buttons, createButton(10, 10, "assets/back.png", goback, "whatcodetype", "barcode"))
-	-- --hehe 
-	-- waveImage = itfbarcode.generateImage("3561413568147977", config)
-	-- lookup = {19.194377939831, 19.451234900763, 19.659473904451, 19.818574461834, 19.928138904377, 19.987893378032, 19.997688527736, 19.957499870716, 19.867427857684, 19.727697621764, 19.538658415776, 19.300782739285, 19.014665157599, 18.681020815666, 18.300683650581, 17.874604307181, 17.403847761927, 16.889590661017, 16.333118379383, 15.735821807925, 15.099193877004, 14.424825824899, 13.714403220536, 12.969701750444, 12.192582780467, 11.384988703313, 10.548938083588, 9.6865206124343, 8.7998918843924, 7.8912680095345, 6.9629200743458, 6.0171684651899, 5.0563770685517, 4.0829473625525, 3.0993124145047, 2.1079307995114, 1.1112804553084, 0.1118524887109, -0.88785505085677, -1.8853434151833, -2.8781194028397, -3.8637015908846, -4.8396265371292, -5.8034549374598, -6.7527777228276, -7.685222080667, -8.598457385691, -9.4902010252409, -10.358224104629, -11.200357018214, -12.014494872287, -12.798602746207, -13.550720778645, -14.268969066215, -14.951552362255, -15.596764564008, -16.202992976988, -16.768722345882, -17.292538641893, -17.773132597079, -18.209302976841, -18.599959582382, -18.944125975639, -19.240941919867, -19.489665529785, -19.689675125899, -19.840470788383, -19.941675606614, -19.993036621252, -19.994425456509, -19.945838641021, -19.847397616521, -19.699348434306, -19.502061140227, -19.256028849774, -18.961866515543, -18.620309390173, -18.232211188596, -17.7985419542, -17.32038563422, -16.798937370437, -16.235500511946, -15.631483357459, -14.988395635299, -14.307844729855, -13.591531663969, -12.841246847255, -12.058865601015, -11.246343470909, -10.405711339107, -9.5390703481479, -8.64858664917, -7.736485987666, -6.8050481402769, -5.8566012165376, -4.8935158398159, -3.9181992219885, -2.9330891466648, -1.9406478759978, -0.94335599631063, 0.1118524887109, 1.1112804553084, 2.1079307995114, 3.0993124145047, 4.0829473625525, 5.0563770685517, 6.0171684651899, 6.9629200743458, 7.8912680095345, 8.7998918843924, 9.6865206124343, 10.548938083588, 11.384988703313, 12.192582780467, 12.969701750444, 13.714403220536, 14.424825824899, 15.099193877004, 15.735821807925, 16.333118379383, 16.889590661017, 17.403847761927, 17.874604307181, 18.300683650581, 18.681020815666, 19.014665157599}
-	-- numSegments = 126 -- Number of vertical slices (higher = smoother wave)
-	-- offsets = {}      -- Table to store the Y offsets for each slice
-	-- local sin = math.sin
-	-- local pi = math.pi
-	-- local imgWidth = waveImage:getWidth()
-	-- local imgHeight = waveImage:getHeight()
-	-- sliceWidth = imgWidth / numSegments
-	-- yPos = love.graphics.getHeight() / 2 - imgHeight / 2
-	-- yOffsetIndex = 0
-	-- -- Precompute quads if the number of segments is fixed
-	-- quads = {}
-	-- logoSpriteBatch = love.graphics.newSpriteBatch(waveImage, numSegments)
-	-- for i = 0, numSegments - 1 do
-		-- local x = i * sliceWidth
-		-- quads[i + 1] = love.graphics.newQuad(x, 0, sliceWidth, imgHeight, imgWidth, imgHeight)
-	-- end
-	-- local function getYOffsetIndex(i)
-		-- return (yOffsetIndex + i) % 126 + 1 -- + 1 bo tablice w lua indexują się od 1
-	-- end
-	-- local function updateYOffsetIndex() yOffsetIndex = (yOffsetIndex + 1) % 126 end
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+		
+	
+	
+		
+	
+	
 	centerX, centerY = SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2
 	for i = 1, numStars do
 		createStar()
@@ -127,7 +127,7 @@ function love.load()
 			x = centerX + math.cos(angle) * distance,
 			y = centerY + math.sin(angle) * distance,
 			distance = distance,
-			speed = love.math.random(50, 150) / 100, -- Speed multiplier for star movement
+			speed = love.math.random(50, 150) / 100, 
 			size = love.math.random(1, 3)
 		}
 	end
@@ -151,7 +151,7 @@ local function isoToUnix(isoDate)
     year, month, day = tonumber(year), tonumber(month), tonumber(day)
     hour, min, sec = tonumber(hour), tonumber(min), tonumber(sec)
 
-    -- Days calculation since the Unix epoch (1970-01-01)
+    
     local days = 0
     for y = 1970, year - 1 do
         days = days + (isLeapYear(y) and 366 or 365)
@@ -163,13 +163,13 @@ local function isoToUnix(isoDate)
 
     days = days + (day - 1)
 
-    -- Convert to total seconds
+    
     local totalSeconds = days * 86400 + hour * 3600 + min * 60 + sec
 
     return totalSeconds
 end
 function createButton(x, y, imagePath, callback, statename, secstatename, thrdstate)
-    local image = love.graphics.newImage(imagePath) -- Load the image
+    local image = love.graphics.newImage(imagePath) 
     return {
         x = x,
         y = y,
@@ -178,13 +178,13 @@ function createButton(x, y, imagePath, callback, statename, secstatename, thrdst
         image = image,
         callback = callback,
         draw = function(self)
-            -- Draw the image as the button
+            
 			if state == statename or state == secstatename then
 				love.graphics.draw(self.image, self.x, self.y, 0, BUTTONSCALE)
 			end
         end,
         isTouched = function(self, touchX, touchY)
-            -- Check if touch is within button boundaries
+            
 			if state == statename or state == secstatename then
 				return touchX > self.x and touchX < self.x + self.width and
 					touchY > self.y and touchY < self.y + self.height
@@ -197,7 +197,7 @@ function calculatetotp()
 
 	local function c(arr, index)
 		local result = 0
-		for i = index, index + 3 do  -- Fix the loop to read 4 bytes correctly
+		for i = index, index + 3 do  
 			result = bit.bor(bit.lshift(result, 8), bit.band(arr:byte(i), 0xFF))
 		end
 		return result
@@ -222,16 +222,16 @@ function calculatetotp()
 	local outputBytes = sha1.hmac_binary(secret, msg)
 
 	if outputBytes ~= nil then
-		-- Ensure we have enough bytes to read
+		
 		if #outputBytes >= 4 then
-			-- Use the last byte of the HMAC result to calculate the offset
+			
 			local byteIndex = outputBytes:byte(#outputBytes)
 			local offset = bit.band(byteIndex, 15)
 			print("byteIndex: " .. byteIndex)
 			print("offset: " .. offset)
 
-			-- Extract the integer at the specified offset to generate the TOTP
-			local magicNumber = bit.band(c(outputBytes, offset + 1), javaIntMax) % 1000000  -- Offset adjusted for 1-based index
+			
+			local magicNumber = bit.band(c(outputBytes, offset + 1), javaIntMax) % 1000000  
 			totp = string.format("%06d", magicNumber)
 			print(totp)
 			print("https://zlgn.pl/view/dashboard?ploy=" .. codes[selectioncode + pagegap].zappkaid .. "&loyal=" .. totp)
@@ -259,26 +259,26 @@ end
 function refresh_data(url, request, inheaders, metoda)
     print(url)
 	print(request)
-	--love.filesystem.write("data.txt", request)
-    -- Headers
-    -- local myheaders = {
-        -- ["user-agent"] = "Mozilla/5.0 (Windows NT 10.0; rv:129.0) Gecko/20100101 Firefox/129.0",
-        -- ["accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/png,image/svg+xml,*/*;q=0.8",
-        -- ["sec-fetch-user"] = "?1",
-		-- ["sec-fetch-site"] = "none",
-        -- ["sec-fetch-mode"] = "navigate",
-        -- ["sec-fetch-dest"] = "document",
-        -- ["accept-encoding"] = "gzip, deflate, br, zstd",
-        -- ["accept-language"] = " pl,en-US;q=0.7,en;q=0.3",
-		-- ["upgrade-insecure-requests"] = "1",
-		-- ["te"] = "trailers",
-		-- ["content-length"] = "0",
-        -- ["priority"] = "u=0, i"
-    -- }
-    -- Response table to collect the response body
-	local request_body = request --the json body
+	
+    
+    
+        
+        
+        
+		
+        
+        
+        
+        
+		
+		
+		
+        
+    
+    
+	local request_body = request 
     response_body = {}
-    -- Making the HTTP request
+    
 
 	code, body, headers = https.request(url, {data = request_body, method = metoda, headers = inheaders})
 	print(body)
@@ -363,7 +363,7 @@ function addcode()
 	selectioncode = 1
 	state = "whatcodetype"
 end
---kod podjebany z żappka3ds lol (no wsm nie podjebany bo to mój kod)
+
 function test()
 	local data = json.encode({idToken = boinaczejjebnie})
     refresh_data("https://www.googleapis.com/identitytoolkit/v3/relyingparty/getAccountInfo?key=AIzaSyDe2Fgxn_8HJ6NrtJtp69YqXwocutAoa9Q", data, {["Content-Type"] = "application/json", ["X-Android-Package"] = "pl.zabka.apb2c", ["X-Android-Cert"] = "FAB089D9E5B41002F29848FC8034A391EE177077", ["Accept-Language"] = "en-US", ["X-Client-Version"] = "Android/Fallback/X22003001/FirebaseCore-Android", ["X-Firebase-GMPID"] = "1:146100467293:android:0ec9b9022788ad32b7bfb4", ["X-Firebase-Client"] = "H4sIAAAAAAAAAKtWykhNLCpJSk0sKVayio7VUSpLLSrOzM9TslIyUqoFAFyivEQfAAAA", ["Content-Length"] = "894", ["User-Agent"] = "Dalvik/2.1.0 (Linux; U; Android 9; SM-A600FN Build/PPR1.180610.011)", ["Host"] = "www.googleapis.com", ["Connection"] = "Keep-Alive"}, "POST")
@@ -380,8 +380,8 @@ end
 function sendbackvercode(smscode)  
 	local data = json.encode({operationName = "SignInWithPhone",variables = {input = {phoneNumber = {countryCode = "48", nationalNumber = numertel},verificationCode = smscode}}, query = "mutation SignInWithPhone($input: SignInInput!) { signIn(input: $input) { customToken } }"})
 	refresh_data("https://super-account.spapp.zabka.pl/", data, {["content-type"] = "application/json", ["authorization"] = "Bearer " .. boinaczejjebnie, ["user-agent"] = "okhttp/4.12.0", ["x-apollo-operation-id"] = "a531998ec966db0951239efb91519560346cfecac77459fe3b85c5b786fa41de"	,["x-apollo-operation-name"] = "SignInWithPhone", ["accept"] = "multipart/mixed; deferSpec=20220824, application/json"}, "POST")
-	--love.filesystem.write("data.txt", data)
-	--love.filesystem.write("debug.txt", body)
+	
+	
 	local tokentemp = responded.data.signIn.customToken
 	local data = json.encode({token = tokentemp, returnSecureToken = "true"})
 	refresh_data("https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=AIzaSyDe2Fgxn_8HJ6NrtJtp69YqXwocutAoa9Q", data, {["content-type"] = "application/json"}, "POST")
@@ -406,7 +406,7 @@ function sendbackvercode(smscode)
 end
 
 function draw_top_screen()
-    -- Fill background
+    
     if theme == "light" then
         love.graphics.setColor(1, 1, 1, 1)
         love.graphics.rectangle("fill", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -419,14 +419,14 @@ function draw_top_screen()
         end 
     end
 	if theme == "dark" then
-		love.graphics.setColor(1,1,1,0.4) -- White color for the stars
+		love.graphics.setColor(1,1,1,0.4) 
 		for _, star in ipairs(stars) do
 			love.graphics.circle("fill", star.x, star.y, 2)
 		end
 	end	
 	if theme == "dark" then
 		love.graphics.setColor(1,1,1,0.7)
-		-- Calculate the position for each letter based on sine wave
+		
 		local offset = love.graphics.getHeight() / 2
 
 		for i = 1, #text do
@@ -452,10 +452,10 @@ function draw_top_screen()
 				love.graphics.draw(barcodeImage, currentX2, y)
 			elseif codeteraz == "ZAPPKA" or codeteraz == "QRCODE" then
 				if qr1 ~= nil then
-					-- Get the size of the QR code
+					
 					local qrSize = qr1:getSize()
 					local scale = QR_SCALE_BASE / qrSize 
-					-- Calculate the position to center the QR code on the screen
+					
 					local x = (SCREEN_WIDTH - qrSize) / 2
 					local y = (SCREEN_HEIGHT - qrSize) / 2
 					love.graphics.setColor(1,1,1,1)
@@ -470,18 +470,18 @@ function draw_top_screen()
 			elseif codeteraz == "EAN13" then
 				love.graphics.setColor(1, 1, 1, 1)
 				EAN13.render_image(barcode_image, SCREEN_WIDTH, SCREEN_HEIGHT, BAR_SCALE)
-			-- elseif codeteraz == "wavetest" then
-				-- love.graphics.setColor(1, 1, 1, 1)
-				-- local imageWidth = barcodeImage:getWidth()
-				-- local currentX2 = (400 - imageWidth) / 2
-				-- logoSpriteBatch:clear()
-				-- for i = 0, numSegments - 1 do
-					-- local yOffset = (lookup[i + 1] or 0) / 2  -- Ensure lookup is not nil            
-				-- -- add the precomputed quad with offset to spritebatch
-					-- logoSpriteBatch:add(quads[i + 1], i*sliceWidth, yPos + yOffset)
-				-- end    
-					-- -- draw the spritebatch    
-				-- love.graphics.draw(logoSpriteBatch, currentX2)
+			
+				
+				
+				
+				
+				
+					
+				
+					
+				
+					
+				
 			end
 		else
 			if love._console == "3DS" then
@@ -533,7 +533,7 @@ function draw_bottom_screen()
     SCREEN_WIDTH = 400
     SCREEN_HEIGHT = 240
 
-    -- Fill background
+    
     if theme == "light" then
         love.graphics.setColor(1, 1, 1, 1)
         love.graphics.rectangle("fill", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -592,10 +592,10 @@ function rendercode()
 	showcode = true
 end
 function love.touchpressed(id, x, y, dx, dy, pressure)
-    -- Check if any button is pressed
+    
     for _, button in ipairs(buttons) do
         if button:isTouched(x, y) then
-            button.callback() -- Call the button's callback
+            button.callback() 
         end
     end
 end
@@ -722,37 +722,37 @@ function createStar()
         x = centerX + math.cos(angle) * distance,
         y = centerY + math.sin(angle) * distance,
         distance = distance,
-        speed = love.math.random(50, 150) / 100, -- Speed multiplier for star movement
+        speed = love.math.random(50, 150) / 100, 
         size = love.math.random(1, 3)
     })
 end
 function love.update(dt)
-	-- if codeteraz == "wavetest" then
-		-- shiftRight(lookup)
-	-- end
+	
+		
+	
 	if theme == "dark" then
 		for i, star in ipairs(stars) do
-			-- Move the star away from the center
+			
 			local directionX = (star.x - centerX) * star.speed * dt
 			local directionY = (star.y - centerY) * star.speed * dt
 			star.x = star.x + directionX
 			star.y = star.y + directionY
 
-			-- Decrease the size of the star as it moves outward to simulate perspective
+			
 			star.size = star.size * (1 - dt * 0.2)
 
-			-- If the star moves off-screen or becomes too small, reset it to the center
+			
 			if star.x < 0 or star.x > SCREEN_WIDTH or star.y < 0 or star.y > SCREEN_HEIGHT or star.size < 0.5 then
-				table.remove(stars, i) -- Remove the star from the list
-				createStar() -- Create a new star at the center
+				table.remove(stars, i) 
+				createStar() 
 			end
 		end
 	end
 	if theme == "dark" then
-		-- Update the x position of the text
+		
 		xPos = xPos - speed * dt
 
-		-- Reset position when the text goes off-screen
+		
 		if xPos < -(#text * charWidth) then
 			xPos = love.graphics.getWidth()
 		end
